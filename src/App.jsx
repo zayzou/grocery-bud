4363c866c1626cf5deae7ee18c590b8851df294a
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-27 21:06:06 -0400

    implement  localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { nanoid } from "nanoid";
 import Form from "./component/Form";
 import Items from "./component/Items";
 const App = () => {
-  const [items, setItems] = useState([]);
-
+  const local = JSON.parse(localStorage.getItem("items")) ?? [];
+  const [items, setItems] = useState(local);
+  console.log(items);
   const addItem = (name) => {
-    const newItem = { id: nanoid(), name: name, completed: false };
-    setItems([...items, newItem]);
+    const newItem = { id:nanoid(), name: name, completed: false };
+    const updatedItems = [...items, newItem];
+    setItems(updatedItems);
+    localStorage.setItem("items", JSON.stringify(updatedItems));
   };
   const removeItem = (id) => {
     setItems(items.filter((item) => item.id !== id));
